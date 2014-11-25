c2f46dc06d5966a0b23afad9357016fe99614cf4
Albert KEBA MAGNAGNA <albert.keba@gmail.com>
2014-11-25 08:31:04 -0500

    heritage view

--- scripts/view.js
+++ scripts/view.js
@@ -1,24 +1,5 @@
 /*jslint devel: true, white: true*/
-/*global App, $*//*
-(function () {
-	//'use strict';
-	
-	this.Class = function(){}
-	
-	Class.extend = function (prop) {
-		function Class() {}
-		
-		Class.prototype = new this();
-		Class.prototype.constructor = Class;
-		Class.extend = arguments.callee;
-		
-		return Class;
-	};
-})();
-
-App.Views.Test = Class.extend({
-	test: function(){}
-});*/
+/*global App, $, Mustache*/
 
 //http://esbueno.noahstokes.com/post/77292606977/self-executing-anonymous-functions-or-how-to-write
 //http://ejohn.org/blog/simple-javascript-inheritance/
@@ -28,34 +9,86 @@ App.Views.Test = Class.extend({
 
 var View = (function () {
 	'use strict';
-	
-	function Class(options) {
+	var _template = null;
+	function Class( options ) {
 		options = options || null;
 		
-		this.template = options.template || null;
+		if (options)
+		{
+			this.template	= options.template || null;
+			this.events		= options.events || null;
+			this.$el		= options.el ? $( options.el ) : null;
+			this.model		= options.model || null;
+		}
 	}
 	
-	Class.extend = function (options) {
+	Class.extend = function ( options ) {
 		var prop,
-			extended = {};
+			extended = {},
+			prototype= new this();
 		
-		for (prop in options) 
+		for ( prop in options ) 
 		{
-			/*if ( Object.prototype.hasOwnProperty.call(options, prop) )
+			if ( Object.prototype.hasOwnProperty.call(options, prop) && Object.prototype.hasOwnProperty.call(prototype, prop) )
 			{
-				extended[prop] = options[prop];
-			}*/
-			
-			console.log(prop, this.prototype[template]);
-			console.log(Object.prototype.hasOwnProperty.call(Class, this[prop]));
+				prototype[prop] = options[prop];
+			}
 		}
+			
+		Class.prototype.render = function(){
+			if ( this.template )
+			{
+				this._template = $( Mustache.render(this.template) );
+				this.bindEvents();
+				
+				if ( this.$el  )
+				{
+					this.$el.prepend( this._template );
+				}
+			}
+		};
+		
+		Class.prototype.bindEvents = function () {
+			var events = this.events, key, el, element = null;
+			
+			for ( key in events )
+			{
+				if ( events.hasOwnProperty(key) )
+				{
+					for ( el in events[key] )
+					{
+						if ( (events[key]).hasOwnProperty(el) )
+						{
+							if ( this._template.prop('tagName').toLowerCase() === events[key][el].element )
+							{
+								element = this._template;
+							}
+							else
+							{
+								element = this._template.find( events[key][el].element, this );
+							}
+
+							element.on( key, this[events[key][el].attach] );
+						}
+					}
+				}
+			}
+		};
+		
+		Class.prototype = prototype;
+		Class.extend = arguments.callee;
+		prototype = null;
+		
+		return Class;
 	};
 	
 	return Class;
 })();
 
-var v = View.extend({
+var V = View.extend({
 	template: 'View'
 });
 
-console.log(v);
+var vv = new V({template: 'Test'});
+
+console.log(vv);
