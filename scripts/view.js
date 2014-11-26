6f4768bea4840b48e147b58bf8f8e9b5ab5b5156
Albert KEBA MAGNAGNA <albert.keba@gmail.com>
2014-11-25 22:29:07 -0500

    heritage

--- scripts/view.js
+++ scripts/view.js
@@ -1,11 +1,5 @@
-/*jslint devel: true, white: true*/
+/*jslint devel: true, nomen: true, white: true*/
 /*global App, $, Mustache*/
-
-//http://esbueno.noahstokes.com/post/77292606977/self-executing-anonymous-functions-or-how-to-write
-//http://ejohn.org/blog/simple-javascript-inheritance/
-//http://krasimirtsonev.com/blog/article/JavaScript-dependency-free-extend-method
-//http://www.htmlgoodies.com/html5/javascript/extending-javascript-objects-in-the-classical-inheritance-style.html#fbid=tlutCPMf82L
-//http://www.htmlgoodies.com/html5/javascript/calling-parent-methods-in-javascript.html#fbid=tlutCPMf82L
 
 var View = (function () {
 	'use strict';
@@ -24,71 +18,87 @@ var View = (function () {
 	
 	Class.extend = function ( options ) {
 		var prop,
-			extended = {},
+			extended= {},
 			prototype= new this();
 		
 		for ( prop in options ) 
 		{
-			if ( Object.prototype.hasOwnProperty.call(options, prop) && Object.prototype.hasOwnProperty.call(prototype, prop) )
+			if ( options.hasOwnProperty( prop) )
 			{
-				prototype[prop] = options[prop];
-			}
-		}
-			
-		Class.prototype.render = function(){
-			if ( this.template )
-			{
-				this._template = $( Mustache.render(this.template) );
-				this.bindEvents();
-				
-				if ( this.$el  )
+				if ( Object.prototype.hasOwnProperty.call(options, prop) )
 				{
-					this.$el.prepend( this._template );
+					prototype[prop] = options[prop];
 				}
-			}
-		};
-		
-		Class.prototype.bindEvents = function () {
-			var events = this.events, key, el, element = null;
-			
-			for ( key in events )
-			{
-				if ( events.hasOwnProperty(key) )
-				{
-					for ( el in events[key] )
-					{
-						if ( (events[key]).hasOwnProperty(el) )
-						{
-							if ( this._template.prop('tagName').toLowerCase() === events[key][el].element )
-							{
-								element = this._template;
-							}
-							else
-							{
-								element = this._template.find( events[key][el].element, this );
-							}
 
-							element.on( key, this[events[key][el].attach] );
-						}
+				if ( /function/.test(options[prop]) )
+				{
+					if ( this.prototype.hasOwnProperty( prop) )
+					{
+						prototype[prop] = options[prop];
 					}
 				}
 			}
-		};
+		}
 		
 		Class.prototype = prototype;
-		Class.extend = arguments.callee;
 		prototype = null;
 		
 		return Class;
+	};
+	
+	Class.prototype.render = function(){
+		if ( this.template )
+		{
+			this._template = $( Mustache.render(this.template) );
+			this.bindEvents();
+
+			if ( this.$el  )
+			{
+				this.$el.prepend( this._template );
+			}
+		}
+	};
+		
+	Class.prototype.bindEvents = function () {
+		var events = this.events, key, el, element = null;
+
+		for ( key in events )
+		{
+			if ( events.hasOwnProperty(key) )
+			{
+				for ( el in events[key] )
+				{
+					if ( (events[key]).hasOwnProperty(el) )
+					{
+						if ( this._template.prop('tagName').toLowerCase() === events[key][el].element )
+						{
+							element = this._template;
+						}
+						else
+						{
+							element = this._template.find( events[key][el].element, this );
+						}
+
+						element.on( key, this[events[key][el].attach] );
+					}
+				}
+			}
+		}
 	};
 	
 	return Class;
 })();
 
 var V = View.extend({
-	template: 'View'
+	template: 'Temp',
+	model: {},
+	render: function(){
+		'use strict';
+		console.log('render child');
+	}
 });
 
-var vv = new V({template: 'Test'});
+var v = new V();
 
-console.log(vv);
+console.log(v);
+v.render();
