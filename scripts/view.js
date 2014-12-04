d764694ff5bdf85692cb69bcdb1ff3669bad40e7
Albert KEBA MAGNAGNA <albert.keba@gmail.com>
2014-12-04 08:36:39 -0500

    heritage

--- scripts/view.js
+++ scripts/view.js
@@ -1,11 +1,51 @@
 /*jslint devel: true, nomen: true, plusplus: true, white: true*/
+/*global $, Mustache*/
 var View = (function () {
 	'use strict';
+	var _template = null;
 	
-	function Class(){}
+	function Class(){
+		console.log('construct Class');
+	}
 	
-	Class.prototype.test = function() {
-		console.log('test View');
+	Class.prototype.render = function () {
+		
+		if ( this.template && this.model )
+		{
+			this._template = $( Mustache.render(this.template, this.model) );
+			this.bindEvents();
+
+			return this._template;
+		}
+
+		return {};
+	};
+	
+	Class.prototype.bindEvents = function() {
+		var events = this.events, key, el, element = null;
+
+		for ( key in events )
+		{
+			if ( events.hasOwnProperty(key) )
+			{
+				for ( el in events[key] )
+				{
+					if ( (events[key]).hasOwnProperty(el) )
+					{
+						if ( this._template.prop('tagName').toLowerCase() === events[key][el].element )
+						{
+							element = this._template;
+						}
+						else
+						{
+							element = this._template.find( events[key][el].element, this );
+						}
+
+						element.on( key, {self:this},this[events[key][el].attach] );
+					}
+				}
+			}
+		}
 	};
 	
 	return Class;
@@ -14,13 +54,11 @@ var View = (function () {
 var Child = (function () {
 	'use strict';
 	
-	function Class(){}
+	function Class(){
+		View.call(this);
+	}
 	
 	Class.prototype = Object.create(View.prototype);
-	
-	/*Class.prototype.test = function(){
-		console.log('test Child');
-	};*/
 	
 	Class.prototype.testbis = function() {
 		console.log('testbis');
@@ -35,6 +73,3 @@ console.log(V);
 
 var C = new Child();
 console.log(C);
-
-C.test();
-C.testbis();
